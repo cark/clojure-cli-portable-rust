72c83e492b5d34bc258d7ca36c0bfc38960b48fc
carkh <carkh@gmail.com>
2019-08-07 15:55:20 +0200

    Unix exec

--- src/unix.rs
+++ src/unix.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::env::VarError;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
+extern crate exec;
 
 pub fn get_args() -> Vec<String> {
     env::args().collect()
@@ -23,3 +24,11 @@ pub fn get_user_cache_dir(config_dir: &str) -> Result<String,  VarError> {
              .to_str().expect("Couldn't convert path")
              .to_owned())
 }
+
+pub fn exec(command: &Path, args: Vec<&str>) -> () {
+    let err = exec::Command::new(command)
+        .args(&args)
+        .exec();
+    println!("Error executing '{:?}' : '{}'", command, err);
+}
+
